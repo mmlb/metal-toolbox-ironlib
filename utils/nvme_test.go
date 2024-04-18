da633b0e8176236532ffefb0c8c609fae6e06816
Manuel Mendez <github@i.m.mmlb.dev>
2024-04-18 15:11:28 -0400

    utils/nvme: Explicitly test parsing fna and sanicap values
    Better to test explicitly for all expected cases than implicitly/by
    proxy from the other tests.

--- utils/nvme_test.go
+++ utils/nvme_test.go
@@ -2,10 +2,12 @@ package utils
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/bmc-toolbox/common"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_NvmeComponents(t *testing.T) {
@@ -67,4 +69,104 @@ var fixtureNvmeDeviceCapabilities = []*common.Capability{
 	{Name: "ber", Description: "Block Erase Sanitize Operation Supported", Enabled: false},
 	{Name: "owr", Description: "Overwrite Sanitize Operation Supported", Enabled: false},
 	{Name: "ndi", Description: "No-Deallocate After Sanitize bit in Sanitize command Supported", Enabled: false},
+}
+
+func Test_NvmeParseFna(t *testing.T) {
+	// These are laid out so if you squint and pretend false/true are 0/1 they match the bit pattern of the int
+	// Its a map so order doesn't matter but I think it makes it easier to match a broken test to the code
+	wants := []map[string]bool{
+		{"cese": false, "cens": false, "fmns": false},
+		{"cese": false, "cens": false, "fmns": true},
+		{"cese": false, "cens": true, "fmns": false},
+		{"cese": false, "cens": true, "fmns": true},
+		{"cese": true, "cens": false, "fmns": false},
+		{"cese": true, "cens": false, "fmns": true},
+		{"cese": true, "cens": true, "fmns": false},
+		{"cese": true, "cens": true, "fmns": true},
+	}
+	for i, want := range wants {
+		t.Run(fmt.Sprintf("0b0...%03b", i), func(t *testing.T) {
+			caps := parseFna(uint(i))
+			require.Len(t, caps, len(want))
+			for _, cap := range caps {
+				require.Equal(t, want[cap.Name], cap.Enabled)
+			}
+		})
+	}
+}
+
+// This test tests parsing sanicap values that we're interested in
+// The test is broken up into 3 sections
+// First it iterates through all the bit patterns for the low bits that we're interested in, namely 0-7 aka 0b000-b111
+// Then it tests the high bits we're interested in: bits 29-31.
+// Finally it tests that if bit 31 (the 32nd bit) is set the function panics because we don't know what to do with that bit's data.
+func Test_NvmeParseSanicap(t *testing.T) {
+	// These are laid out so if you squint and pretend false/true are 0/1 they match the bit pattern of the int
+	// Its a map so order doesn't matter but I think it makes it easier to match a broken test to the code
+
+	// lower bits only
+	wants := []map[string]bool{
+		{"owr": false, "ber": false, "cer": false},
+		{"owr": false, "ber": false, "cer": true},
+		{"owr": false, "ber": true, "cer": false},
+		{"owr": false, "ber": true, "cer": true},
+		{"owr": true, "ber": false, "cer": false},
+		{"owr": true, "ber": false, "cer": true},
+		{"owr": true, "ber": true, "cer": false},
+		{"owr": true, "ber": true, "cer": true},
+	}
+	for i, want := range wants {
+		// not testing ndi yet but its being returned
+		// don't want to add it above to avoid noise
+		want["ndi"] = false
+		t.Run(fmt.Sprintf("0b000...%03b", i), func(t *testing.T) {
+			caps, err := parseSanicap(uint(i))
+			require.NoError(t, err)
+			require.Len(t, caps, len(want))
+			for _, cap := range caps {
+				require.Equal(t, want[cap.Name], cap.Enabled)
+			}
+		})
+	}
+
+	// higher bits only
+	wants = []map[string]bool{
+		{}, // already tested above and want to avoid t.Run duplicate name handling
+		{"ndi": true},
+		{"nodmmas": false, "ndi": false},
+		{"nodmmas": false, "ndi": true},
+		{"nodmmas": true, "ndi": false},
+		{"nodmmas": true, "ndi": true},
+	}
+	for i, want := range wants {
+		if i == 0 {
+			// already tested above
+			continue
+		}
+		// not testing these now but they are being returned
+		// don't want to add them above to avoid noise
+		want["owr"] = false
+		want["ber"] = false
+		want["cer"] = false
+		i = (i << 29)
+		name := fmt.Sprintf("%032b", i)
+		name = "0b" + name[:3] + "...000"
+		t.Run(name, func(t *testing.T) {
+			caps, err := parseSanicap(uint(i))
+			require.NoError(t, err)
+			require.Len(t, caps, len(want))
+			for _, cap := range caps {
+				require.Equal(t, want[cap.Name], cap.Enabled)
+			}
+		})
+	}
+
+	i := 0b11 << 30
+	name := fmt.Sprintf("%032b", i)
+	name = "0b" + name[:3] + "...000"
+	t.Run(name, func(t *testing.T) {
+		caps, err := parseSanicap(uint(i))
+		require.Error(t, err)
+		require.Nil(t, caps)
+	})
 }
